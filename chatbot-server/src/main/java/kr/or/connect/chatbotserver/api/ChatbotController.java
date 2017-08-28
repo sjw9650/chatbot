ec230c3a49a2cfb57f1049029bc0a638c20fdde8
rrljdy <oil_oil@naver.com>
2017-08-29 01:09:29 +0900

    notice crawring

--- chatbot-server/src/main/java/kr/or/connect/chatbotserver/api/ChatbotController.java
+++ chatbot-server/src/main/java/kr/or/connect/chatbotserver/api/ChatbotController.java
@@ -1,5 +1,10 @@
 package kr.or.connect.chatbotserver.api;
+import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
@@ -12,7 +17,7 @@ public class ChatbotController {
     @RequestMapping(value = "/keyboard", method = RequestMethod.GET)
     public String keyboard() {
 
-        System.out.println("/keyboard");    
+        System.out.println("/keyboard");
         JSONObject jobjBtn = new JSONObject();
         jobjBtn.put("type", "buttons");
         ArrayList<String> btns = new ArrayList<>();
@@ -27,8 +32,8 @@ public class ChatbotController {
     // 메세지
     @RequestMapping(value = "/message", method = RequestMethod.POST, headers = "Accept=application/json")
     @ResponseBody
-    public String message(@RequestBody JSONObject resObj) {
-		
+    public String message(@RequestBody JSONObject resObj) throws Exception{
+
         System.out.println("/message");
         System.out.println(resObj.toJSONString());
 
@@ -36,7 +41,6 @@ public class ChatbotController {
         content = (String) resObj.get("content");
         JSONObject jobjRes = new JSONObject();
         JSONObject jobjText = new JSONObject();
-
         // 메시지 구현 
         if(content.contains("안녕")){
             jobjText.put("text","안녕 하세요");
@@ -47,8 +51,21 @@ public class ChatbotController {
         } else if(content.equals("알림")){
             jobjText.put("text","준비 중 입니다...");
         } else if(content.equals("공지사항")){
-            jobjText.put("text","준비 중 입니다...");
-        }else if(content.equals("시작하기")){
+            jobjText.put("text","사용법은 다음과 같습니다. " +
+                    "(굿)\n취업관련 사항은 \"취업\" " +
+                    "장학금관련 사항은 \"장학금\" " +
+                    "일반관련 사항은 \"일반\" " +
+                    "행사관련 사항은 \"행사\"를 입력해주세요." );
+        } else if(content.equals("취업")){
+            noticeCrawling("취업",jobjText);
+        } else if(content.equals("장학금")){
+            noticeCrawling("장학금",jobjText);
+        } else if(content.equals("일반")){
+            noticeCrawling("일반",jobjText);
+        } else if(content.equals("행사")){
+            noticeCrawling("행사",jobjText);
+        }
+        else if(content.equals("시작하기")){
             jobjText.put("text","사용법은 다음과 같습니다. (굿)\n학교 공지사항을 보고 싶으면 \"공지사항\"를 입력해주세요.");
         }else
         {
@@ -60,12 +77,39 @@ public class ChatbotController {
 
         return  jobjRes.toJSONString();
     }
-	
-	//사용자가 옐로아이디를 친구추가했을 때 호출되는 API
-	@RequestMapping(value = "/friend", method = RequestMethod.POST, headers = "Accept=application/json")
-	public String addKakaoFriend(@RequestBody JSONObject resObj) 
-	{
-		System.out.println(resObj.toJSONString());
-		return resObj.toJSONString();
-	}
+
+    //사용자가 옐로아이디를 친구추가했을 때 호출되는 API
+    @RequestMapping(value = "/friend", method = RequestMethod.POST, headers = "Accept=application/json")
+    public String addKakaoFriend(@RequestBody JSONObject resObj)
+    {
+        System.out.println(resObj.toJSONString());
+        return resObj.toJSONString();
+    }
+
+    public void noticeCrawling(String subject,JSONObject jobjTest) throws  Exception{
+        String URL="";
+        if(subject.equals("취업")){
+            URL = "http://www.inu.ac.kr/user/boardList.do?boardId=49235&siteId=mobile&id=mobile_060202000000";
+        }else if(subject.equals("장학금")){
+            URL = "http://www.inu.ac.kr/user/boardList.do?boardId=49227&siteId=mobile&id=mobile_060205000000";
+        }else if(subject.equals("일반")){
+            URL = "http://www.inu.ac.kr/user/boardList.do?boardId=49219&siteId=mobile&id=mobile_060206000000";
+        }else if(subject.equals("행사")){
+            URL = "http://www.inu.ac.kr/user/boardList.do?boardId=49211&siteId=mobile&id=mobile_060207000000";
+        }
+        Document doc = Jsoup.connect(URL).get();
+        Elements links = doc.select("div.tab_cont ul li");
+        JSONArray arr = new JSONArray();
+        for (Element link : links) {
+            Elements attr = link.select("a[href]");
+            Elements img = link.select("img[src]");
+            if (!(img.isEmpty())) {
+                JSONObject data = new JSONObject();
+                data.put("label", attr.text());
+                data.put("url","http://www.inu.ac.kr/user/" + attr.attr("href"));
+                arr.add(data);
+            }
+        }
+        jobjTest.put("message_button",arr);
+    }
 }
