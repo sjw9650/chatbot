8da1e1c05adb992cf9c265f7edf037e04652aa81
Jinwook <sjw9650@gmail.com>
2017-09-02 01:57:25 +0900

    add User

--- chatbot-server/src/main/java/kr/or/connect/chatbotserver/api/ChatbotController.java
+++ chatbot-server/src/main/java/kr/or/connect/chatbotserver/api/ChatbotController.java
@@ -120,9 +120,10 @@ public class ChatbotController {
 
     //사용자가 옐로아이디를 친구추가했을 때 호출되는 API
 	
-    @RequestMapping(value = {"/friend"}, method = RequestMethod.POST, headers = "Accept=application/json")
+    @RequestMapping(value = "/friend", method = RequestMethod.POST, headers = "Accept=application/json")
     public String addKakaoFriend(@RequestBody JSONObject resObj) throws Exception
     {
+        System.out.println("/frinde");
 		String user_key;
         user_key = (String) resObj.get("user_key");
 
