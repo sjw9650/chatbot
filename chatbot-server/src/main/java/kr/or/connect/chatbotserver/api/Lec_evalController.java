c48d28aba21d7cdfa80b64f63bc61045e8fdf319
jhoon07 <wecube.1@gmail.com>
2017-09-05 19:01:40 +0900

    make api file

---
+++ chatbot-server/src/main/java/kr/or/connect/chatbotserver/api/Lec_evalController.java
@@ -0,0 +1,23 @@
+package kr.or.connect.chatbotserver.api;
+
+import kr.or.connect.chatbotserver.lost.ImageGet;
+import kr.or.connect.chatbotserver.model.User;
+import kr.or.connect.chatbotserver.service.LostService;
+import kr.or.connect.chatbotserver.service.UserService;
+import org.json.simple.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.io.IOException;
+import java.text.SimpleDateFormat;
+import java.util.*;
+
+@Controller
+public class Lec_evalController {
+
+    
+
+}
+
+
