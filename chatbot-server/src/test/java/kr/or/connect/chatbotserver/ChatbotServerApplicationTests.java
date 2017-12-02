286250d9c08dfeba2c65cd24a5cffc13df554a73
rrljdy <oil_oil@naver.com>
2017-12-02 18:30:26 +0900

    cafeteria_changed

--- chatbot-server/src/test/java/kr/or/connect/chatbotserver/ChatbotServerApplicationTests.java
+++ chatbot-server/src/test/java/kr/or/connect/chatbotserver/ChatbotServerApplicationTests.java
@@ -1,16 +1,36 @@
 package kr.or.connect.chatbotserver;
 
+import kr.or.connect.chatbotserver.model.CafeteriaManagement;
+import kr.or.connect.chatbotserver.model.CafeteriaMenu;
+import kr.or.connect.chatbotserver.service.CafeteriaMenuService;
+import kr.or.connect.chatbotserver.service.ScheduleService;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ChatbotServerApplicationTests {
 
+	@Autowired
+	ScheduleService scheduleService;
+
+	@Autowired
+	CafeteriaMenuService cafeteriaMenuService;
 	@Test
-	public void contextLoads() {
+	public void contextLoads() throws IOException {
+
 	}
 
 }
