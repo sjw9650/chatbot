80a3ca0cba5c0b20b8cdc94d19e702e604a29552
Jinwook <sjw9650@gmail.com>
2017-12-25 01:15:27 +0900

    modify error

--- chatbot-server/src/main/java/kr/or/connect/chatbotserver/sql/CafeteriaMenuSqls.java
+++ chatbot-server/src/main/java/kr/or/connect/chatbotserver/sql/CafeteriaMenuSqls.java
@@ -5,5 +5,5 @@ import com.fasterxml.jackson.annotation.JsonIgnore;
 public class CafeteriaMenuSqls {
     public static final String SELECT_ALL = "FROM CafeteriaMenu WHERE day like (:date)";
     public static final String DELETE_ALL = "delete from ";
-    public static final String FIND_MENU = "FROM cafeteria_menus where day like (:date) and menu = (:menu)";
+    public static final String FIND_MENU = "FROM CafeteriaMenu where day like (:date) and menu = (:menu)";
 }
